55fbb3db2af9351169e438118a1e1aa46e6bd308
shreyashsng <shreyashsingh1441@gmail.com>
2025-09-24 00:30:12 +0530

    update: register page ui

--- client/src/pages/Register.jsx
+++ client/src/pages/Register.jsx
@@ -19,15 +19,20 @@ const Register = () => {
     }
 
     return (
-        <div>
-            <div>
-                <h2>Register</h2>
-                <form onSubmit={handleSubmit}>
-                    <input type='email' placeholder='Enter email' value={email} onChange={(e) => setEmail(e.target.value)}></input>
-                    <input type='password' placeholder='Enter password' value={password} onChange={(e) => setPassword(e.target.value)}></input>
-                    <button className='bg-green-600 text-white'>Register</button>
+        <div className='flex items-center justify-center min-h-screen bg-gray-50'>
+            <div className='w-full max-w-md bg-white rounded-2xl shadow-lg p-8'>
+                <h2 className='text-3xl font-bold mb-6 text-center text-gray-800'>Register</h2>
+                <form onSubmit={handleSubmit} className='space-y-6'>
+                    <input type='email' placeholder='Enter email' value={email} onChange={(e) => setEmail(e.target.value)} className='w-full px-4 rounded-xl py-4 border border-gray-300'></input>
+                    <input type='password' placeholder='Enter password' value={password} onChange={(e) => setPassword(e.target.value)} className='w-full px-4 rounded-xl py-4 border border-gray-300'></input>
+                    <button className='w-full font-semibold bg-green-600 hover:bg-green-700 transition dureation-200 text-white rounded-xl p-2'>Register</button>
                 </form>
-                {message && <p className='text-center'>{message}</p>}
+                {message && <p className='mt-4 text-center text-green-600 font-medium'>{message}</p>}
+
+                <p className='mt-6 text-center text-gray-600 text-sm'>
+                    Already have an account?{" "}
+                    <a href='/login' className='text-green-600 font-semibold hover:underline'>Login</a>
+                </p>
             </div>
         </div>
     )
